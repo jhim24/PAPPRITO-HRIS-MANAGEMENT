f3ad45193ac5f2bed7eb32ed4cd7b32c7f4e34d9
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-31 00:11:36 +0300

    Create firebase-config.js

---
+++ database/firebase-config.js
@@ -0,0 +1,54 @@
+/* ==========================================
+   PAPPRITO HRIS
+   FIREBASE CONFIG
+========================================== */
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+/* ==========================================
+   FIREBASE CONFIGURATION
+========================================== */
+
+const firebaseConfig = {
+
+    apiKey: "AIzaSyAehoq0teVYHiJ4bkKOgBqIgJZrQpce3k8",
+
+    authDomain: "hr-system-38fc3.firebaseapp.com",
+
+    projectId: "hr-system-38fc3",
+
+    storageBucket: "hr-system-38fc3.firebasestorage.app",
+
+    messagingSenderId: "615471610834",
+
+    appId: "1:615471610834:web:a0d671d4e3f4c1b57b660b"
+
+};
+
+/* ==========================================
+   INITIALIZE FIREBASE
+========================================== */
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
+
+/* ==========================================
+   EXPORT
+========================================== */
+
+export {
+
+    app,
+
+    auth,
+
+    db
+
+};
