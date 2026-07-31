dfc491469e361de7c0ab3c1df6ab41fdc3221890
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-07-31 11:54:30 +0300

    Update employee.js

--- assets/js/employee.js
+++ assets/js/employee.js
@@ -1,1 +1,74 @@
+import { db } from "../../database/firebase-config.js";
 
+import {
+    collection,
+    addDoc,
+    getDocs,
+    deleteDoc,
+    updateDoc,
+    doc,
+    serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.13.0/firebase-firestore.js";
+
+/* ==========================================
+   PAPPRITO HRIS
+   EMPLOYEE MODULE
+========================================== */
+
+const modal = document.getElementById("employeeModal");
+
+const addBtn = document.getElementById("addEmployeeBtn");
+
+const closeBtn = document.getElementById("closeModal");
+
+const cancelBtn = document.getElementById("cancelModal");
+
+/* OPEN MODAL */
+
+addBtn.addEventListener("click",()=>{
+
+    modal.style.display="flex";
+
+});
+
+/* CLOSE MODAL */
+
+closeBtn.addEventListener("click",()=>{
+
+    modal.style.display="none";
+
+});
+
+cancelBtn.addEventListener("click",()=>{
+
+    modal.style.display="none";
+
+});
+
+/* CLICK OUTSIDE */
+
+window.addEventListener("click",(e)=>{
+
+    if(e.target===modal){
+
+        modal.style.display="none";
+
+    }
+
+});
+
+/* ==========================================
+   FIRESTORE
+========================================== */
+
+const employeeCollection = collection(db, "employees");
+
+/* MOBILE SIDEBAR */
+
+const sidebar = document.querySelector(".sidebar");
+
+window.toggleSidebar=function(){
+
+    sidebar.classList.toggle("active");
+
+};
